a37ff0cd2a7823e5c00aa2bb23990f2c5c78b17e
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2020-10-14 10:44:42 -0300

    v1.2

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ summonerForm.children[0].value = 'Grizzo'
 warning.style.opacity = '0'
 
 /**
- * champion
+ * champion did
  * lane
- * kda
+ * kda 
  * goldEarned
  * minionsPerMinute
  * goldPerMinute
@@ -16,6 +16,7 @@ warning.style.opacity = '0'
 
 const getSummoner = async summonerName => {
   const r = await fetch('https://cors-anywhere.herokuapp.com/https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ summonerName +'?api_key='+ apiKey)
+  .catch(e => console.log(e))
   if(r.status !== 200){
     return null
   }
@@ -32,6 +33,7 @@ const getSummoner = async summonerName => {
 
 const getSummonerMatchHistory = async accountId => {
   const r = await fetch('https://cors-anywhere.herokuapp.com/https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/'+ accountId +'?queue=420&api_key='+ apiKey)
+  .catch(e => console.log(e))
   if(r.status !== 200){
     return null
   }
@@ -48,6 +50,7 @@ const getSummonerMatchHistory = async accountId => {
 
 const getMatchInfo = async matchId => {
   const r = await fetch('https://cors-anywhere.herokuapp.com/https://br1.api.riotgames.com/lol/match/v4/matches/'+ matchId +'?api_key='+ apiKey)
+  .catch(e => console.log(e))
   if(r.status !== 200){
     return null
   }
@@ -65,9 +68,11 @@ const getMatchInfo = async matchId => {
 const getMatchStats = (matchInfo, user) => {
   const participantId = matchInfo.participantIdentities.filter(p => p.player.summonerId == user.id)[0].participantId   
   const playerInfo = matchInfo.participants[participantId - 1]
-  const playerLane = [playerInfo.timeline.role, playerInfo.timeline.lane]
-  const champion = dicChampioins[matchInfo.participants[participantId - 1].championId]
-  const win = matchInfo.participants[participantId - 1].stats.win
+  // const playerLane = [playerInfo.timeline.role, playerInfo.timeline.lane]
+  // const kda = [playerInfo.stats.kills, playerInfo.stats.deaths, playerInfo.stats.assists]
+  const champion = dicChampioins[playerInfo.championId]
+  const win = playerInfo.stats.win
+  console.log(playerInfo)
   return {champion, win}
 }
 
@@ -82,25 +87,30 @@ summonerForm.addEventListener('submit', async e => {
   console.log('history')
 
   const champions = {}
+  const limit = 2
   for(let i = 0; i < matchHistory.matches.length; i++){
     const matchInfo = await getMatchInfo(matchHistory.matches[i].gameId)
     const matchStats = getMatchStats(matchInfo, summoner)
     if(!Object.keys(champions).includes(matchStats.champion)){
       champions[matchStats.champion] = {matches: 1}
       champions[matchStats.champion].wins = 0
+      // champions[matchStats.champion].kdas = []
     }else{
       champions[matchStats.champion].matches += 1
     }    
     if(matchStats.win){
       champions[matchStats.champion].wins += 1
     }
+    // champions[matchStats.champion].kdas = [...champions[matchStats.champion].kdas, matchStats.kda]
     champions[matchStats.champion].winRate = Math.ceil(champions[matchStats.champion].wins / champions[matchStats.champion].matches * 100)
-    console.log(i)
-    if(i >= 50){
+
+    console.log(Math.ceil(i / limit * 100) + '%')
+    if(i >= limit - 1){
       break
     }
   }
   console.log(champions)
+
 
   console.log('matches')  
   warning.style.opacity = '0'
